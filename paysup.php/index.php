e846c51e9922384a9cde781fa2fe4390affbde66
PAYSUP <137467409+paysup@users.noreply.github.com>
2023-07-05 04:46:47 -0400

    PAYSUP
    Paysup es una plataforma  en línea establecida con  experiencia en el mercado.  La plataforma ha sido utilizada por miles de personas para generar ingresos y ha recibido críticas positivas de la comunidad en línea. Paysup está diseñado para automatizar muchas de las tareas necesarias para generar ingresos

---
+++ paysup.php/index.php
@@ -0,0 +1,15 @@
+<?php>  
+ echo "pagina de web de paysup"
+  
+ <?>
+  <!DOCTYPE html>
+<html>
+<head>
+<title>paysup</title>
+</head>
+<body>
+<img src="paysup.jpg" alt="paysup" />
+<br />
+<button type="button" onclick="window.location.href='https://www.sys.paysupgo.com/'">Visita mi web de paysup</button>
+</body>
+</html>
